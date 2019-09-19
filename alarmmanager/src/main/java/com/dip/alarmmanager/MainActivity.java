a245957a60c4a7ebbb5b3c8fa2a6067d2d3b112e
Diptendulkar <diptendu.tu@gmail.com>
2019-09-19 18:47:47 +0530

    Alarm Manager- Repeat Alarm

--- alarmmanager/src/main/java/com/dip/alarmmanager/MainActivity.java
+++ alarmmanager/src/main/java/com/dip/alarmmanager/MainActivity.java
@@ -6,23 +6,32 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button start;
+    Button start, repeat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         start= findViewById(R.id.button);
+        repeat= findViewById(R.id.repeatalarm);
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startAlert();
+            }
+        });
+
+        repeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startRepeatAlarm();
             }
         });
     }
@@ -41,5 +50,23 @@ public class MainActivity extends AppCompatActivity {
         alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
                 + (i * 1000), pendingIntent);
         Toast.makeText(this, "Alarm set in " + i + " seconds",Toast.LENGTH_LONG).show();
+    }
+
+    public void startRepeatAlarm(){
+
+        EditText text = findViewById(R.id.time);
+        int interval = 30; // every 30 seconds
+
+        if(!text.getText().toString().isEmpty())
+            interval = Integer.parseInt(text.getText().toString());
+        Intent intent = new Intent(this, MyBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                this.getApplicationContext(), 234324243, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        Toast.makeText(this, "Alarm set in " + interval + " seconds",Toast.LENGTH_LONG).show();
+        alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + (interval * 1000),
+                (interval * 1000), pendingIntent);
     }
 }
