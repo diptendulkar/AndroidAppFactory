7ba3024ad4aa6eee3f143ab8e3eb1193e54f02c9
Diptendulkar <diptendu.tu@gmail.com>
2019-09-19 13:53:19 +0530

    Added Alarm Manager Module

--- app/src/main/java/com/dip/androidappfactory/MainActivity.java
+++ app/src/main/java/com/dip/androidappfactory/MainActivity.java
@@ -3,6 +3,12 @@ package com.dip.androidappfactory;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ListView listview = (ListView) findViewById(R.id.listview);
+
+        /*Add all Modules here*/
+        String[] values = new String[] { "Alarm Manager"   };
+
+       /* final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }*/
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        listview.setAdapter(adapter);
     }
 }
