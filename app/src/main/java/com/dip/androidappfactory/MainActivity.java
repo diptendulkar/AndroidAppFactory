481167e9a617e5d0f9a1e7e5fce192863ec05a01
diptendu <diptendu.tu@gmail.com>
2019-12-23 12:53:22 +0530

    Link module with MainActivity

--- app/src/main/java/com/dip/androidappfactory/MainActivity.java
+++ app/src/main/java/com/dip/androidappfactory/MainActivity.java
@@ -2,12 +2,16 @@ package com.dip.androidappfactory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-
+import android.widget.Toast;
+ import com.dip.alarmmanager.MainActivity;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         final ListView listview = (ListView) findViewById(R.id.listview);
 
         /*Add all Modules here*/
-        String[] values = new String[] { "Alarm Manager"   };
+        String[] values = new String[] { "Alarm Manager" ,"Profile Photo"  };
 
        /* final ArrayList<String> list = new ArrayList<String>();
         for (int i = 0; i < values.length; ++i) {
@@ -28,5 +32,26 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, values);
         listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String itemAtPosition  = (String)parent.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),itemAtPosition,Toast.LENGTH_LONG).show();
+                if(itemAtPosition.equals("Alarm Manager")){
+
+                     Intent launchIntent = new Intent(view.getContext(), com.dip.alarmmanager.MainActivity.class);
+                  //  launchIntent.setClassName("com.dip.alarmmanager", "MainActivity");
+                    startActivity(launchIntent);
+//                    Intent intent = new Intent(getCallingActivity() ,com.dip.alarmmanager.MainActivity.class);
+                  //  Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.dip.alarmmanager.MainActivity");
+                  /*  if (launchIntent != null) {
+                        startActivity(launchIntent);//null pointer check in case package name was not found
+                    }*/
+                }
+                else if(itemAtPosition.equals("Profile Photo")){
+
+                }
+            }
+        });
     }
 }
