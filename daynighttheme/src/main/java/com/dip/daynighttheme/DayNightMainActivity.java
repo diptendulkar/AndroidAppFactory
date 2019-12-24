c3cd18585fa96a283ddf8418926a2914c0c0dcc7
diptendu <diptendu.tu@gmail.com>
2019-12-24 12:02:27 +0530

    initial comit of Day Night Module

---
+++ daynighttheme/src/main/java/com/dip/daynighttheme/DayNightMainActivity.java
@@ -0,0 +1,14 @@
+package com.dip.daynighttheme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DayNightMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_night_main);
+    }
+}
